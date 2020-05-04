65e844af87e6fa856648c15e2d5ec74f94243783
okotar <goutape@gmail.com>
2020-05-04 16:23:58 +0900

    #12 Fix redirects creation

--- gatsby-node.js
+++ gatsby-node.js
@@ -48,6 +48,16 @@ exports.createPages = async ({ graphql, actions }) => {
       },
     })
   })
+
+  // Create redirects.
+  const { createRedirect } = actions
+
+  createRedirect({
+    fromPath: 'https://vibrant-goodall-ecf100.netlify.app/*',
+    toPath:'https://ocoboy.com/:splat',
+    isPermanent: true,
+    force: true,
+  })
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
@@ -61,15 +71,4 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
       value,
     })
   }
-}
-
-exports.createPages = ({ graphql, actions }) => {
-  const { createRedirect } = actions
-
-  createRedirect({
-    fromPath: 'https://vibrant-goodall-ecf100.netlify.app/*',
-    toPath:'https://ocoboy.com/:splat',
-    isPermanent: true,
-    force: true,
-  })
 }
