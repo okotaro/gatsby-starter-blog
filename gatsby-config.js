4ab9198aa582be1368f8521c0c16df4b242e9e98
okotar <goutape@gmail.com>
2020-05-04 18:45:49 +0900

    #21 Add code title

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,7 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          `gatsby-remark-prismjs-title`,
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
