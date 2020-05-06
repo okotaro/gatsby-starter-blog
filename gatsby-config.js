e22766cec640314e9e89d8245eea07bf629b3728
okotaro <goutape@gmail.com>
2020-05-06 15:41:56 +0900

    #26 Add robots txt (#46)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,14 @@
+// disable crawlers for deploy-previews on Netlify
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = 'https://vibrant-goodall-ecf100.netlify.app',
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV
+} = process.env;
+const isNetlifyProduction = NETLIFY_ENV === 'production';
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
+
+
 module.exports = {
   siteMetadata: {
     title: `ocoboy`,
@@ -119,6 +130,27 @@ module.exports = {
       resolve: `gatsby-plugin-canonical-urls`,
       options: {
         siteUrl: `https://ocoboy.com`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: '*' }]
+          },
+          'branch-deploy': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null
+          },
+          'deploy-preview': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null
+          },
+        },
       },
     },
   ],
